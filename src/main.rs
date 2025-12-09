3f3a4ed8cb2ab739dcda16f05fa4ca1102eb4840
haruki7049 <tontonkirikiri@gmail.com>
2025-12-09 12:46:46 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+fn efi_main() {
+    //println!("Hello, world!");
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
