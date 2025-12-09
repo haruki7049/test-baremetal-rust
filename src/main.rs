deb6fda0e77c03d017629e2a9d6b76d878da9512
haruki7049 <tontonkirikiri@gmail.com>
2025-12-09 18:07:40 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@
 use core::mem::{offset_of, size_of};
 use core::panic::PanicInfo;
 use core::ptr::null_mut;
-use core::slice;
+use core::cmp::min;
 
 type EfiVoid = u8;
 type EfiHandle = u64;
+type Result<T> = core::result::Result<T, &'static str>;
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
 #[must_use]
@@ -19,15 +20,14 @@ enum EfiStatus {
 
 #[no_mangle]
 fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
-    let efi_graphics_output_protocol = locate_graphic_protocol(efi_system_table).unwrap();
-    let vram_addr = efi_graphics_output_protocol.mode.frame_buffer_base;
-    let vram_byte_size = efi_graphics_output_protocol.mode.frame_buffer_size;
-    let vram = unsafe {
-        slice::from_raw_parts_mut(vram_addr as *mut u32, vram_byte_size / size_of::<u32>())
-    };
+    let mut vram = init_vram(efi_system_table).expect("init_vram failed");
 
-    for e in vram {
-        *e = 0xffffff;
+    for y in 0..vram.height {
+        for x in 0..vram.width {
+            if let Some(pixel) = vram.pixel_at_mut(x, y) {
+                *pixel = 0x00ff00;
+            }
+        }
     }
 
     loop {
@@ -106,7 +106,7 @@ const _: () = assert!(size_of::<EfiGraphicsOutputProtocolPixelInfo>() == 36);
 
 fn locate_graphic_protocol<'a>(
     efi_system_table: &EfiSystemTable,
-) -> Result<&'a EfiGraphicsOutputProtocol<'a>, &'static str> {
+) -> Result<&'a EfiGraphicsOutputProtocol<'a>> {
     let mut graphic_output_protocol = null_mut::<EfiGraphicsOutputProtocol>();
     let status = (efi_system_table.boot_services.locate_protocol)(
         &EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID,
@@ -124,4 +124,73 @@ fn locate_graphic_protocol<'a>(
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
+}
+
+trait Bitmap {
+    fn bytes_per_pixel(&self) -> i64;
+    fn pixels_per_line(&self) -> i64;
+    fn width(&self) -> i64;
+    fn height(&self) -> i64;
+    fn buf_mut(&mut self) -> *mut u8;
+
+    /// # Safety
+    ///
+    /// Returned pointer is valid as long as the given coordinates are valid
+    /// which means that passing is_in_*_range tests.
+    unsafe fn unchecked_pixel_at_mut(&mut self, x: i64, y: i64) -> *mut u32 {
+        self.buf_mut().add(
+            ((y * self.pixels_per_line() + x) * self.bytes_per_pixel())
+                as usize,
+        ) as *mut u32
+    }
+    fn pixel_at_mut(&mut self, x: i64, y: i64) -> Option<&mut u32> {
+        if self.is_in_x_range(x) && self.is_in_y_range(y) {
+            unsafe { Some(&mut *(self.unchecked_pixel_at_mut(x, y))) }
+        } else {
+            None
+        }
+    }
+
+    fn is_in_x_range(&self, px: i64) -> bool {
+        0 <= px && px < min(self.width(), self.pixels_per_line())
+    }
+    fn is_in_y_range(&self, py: i64) -> bool {
+        0 <= py && py < self.height()
+    }
+}
+
+#[derive(Clone, Copy)]
+struct VramBufferInfo {
+    buf: *mut u8,
+    width: i64,
+    height: i64,
+    pixels_per_line: i64,
+}
+
+impl Bitmap for VramBufferInfo {
+    fn bytes_per_pixel(&self) -> i64 {
+        4
+    }
+    fn pixels_per_line(&self) -> i64 {
+        self.pixels_per_line
+    }
+    fn width(&self) -> i64 {
+        self.width
+    }
+    fn height(&self) -> i64 {
+        self.height
+    }
+    fn buf_mut(&mut self) -> *mut u8 {
+        self.buf
+    }
+}
+
+fn init_vram(efi_system_table: &EfiSystemTable) -> Result<VramBufferInfo> {
+    let gp = locate_graphic_protocol(efi_system_table)?;
+    Ok(VramBufferInfo {
+        buf: gp.mode.frame_buffer_base as *mut u8,
+        width: gp.mode.info.horizontal_resolution as i64,
+        height: gp.mode.info.vertical_resolution as i64,
+        pixels_per_line: gp.mode.info.pixels_per_scan_line as i64,
+    })
 }
