77ec2e2dda725d386b4a0604b4a76a6d9028c415
haruki7049 <tontonkirikiri@gmail.com>
2026-04-26 16:58:54 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,21 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
         let _ = draw_line(&mut vram, 0xff0000, cx, cy, i, rect_size);
     }
 
-    let font_a = "
+    for (i, c) in "ABCDEF".chars().enumerate() {
+        let x = i as i64 * 16 + 256;
+        let y = i as i64 * 16;
+        let color = 0xffffff;
+        draw_font_fg(&mut vram, x, y, color, c)
+    }
+
+    loop {
+        hlt();
+    }
+}
+
+fn draw_font_fg<T: Bitmap>(buf: &mut T, x: i64, y: i64, color: u32, c: char) {
+    if let Ok(_c) = u8::try_from(c) {
+        let font_a = "
 ........
 ...**...
 ...**...
@@ -62,18 +76,15 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
 ........
 ........
     ";
-    for (y, row) in font_a.trim().split('\n').enumerate() {
-        for (x, pixel) in row.chars().enumerate() {
-            let color = match pixel {
-                '*' => 0xffffff,
-                _ => continue,
-            };
-            let _ = draw_point(&mut vram, color, x as i64, y as i64);
+        for (dy, row) in font_a.trim().split('\n').enumerate() {
+            for (dx, pixel) in row.chars().enumerate() {
+                let color = match pixel {
+                    '*' => color,
+                    _ => continue,
+                };
+                let _ = draw_point(buf, color, x + dx as i64, y + dy as i64);
+            }
         }
-    }
-
-    loop {
-        hlt();
     }
 }
 
